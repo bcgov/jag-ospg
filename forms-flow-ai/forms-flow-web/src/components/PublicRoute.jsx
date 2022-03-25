85f3ba64d599cb0871ee7ba70cc5bc255ddae5e2
Andre Pestana <andre.pestana@aot-technologies.com>
2022-03-25 15:07:32 -0700

    Renamed forms-flow-vue-web; add forms-flow-web

---
+++ forms-flow-ai/forms-flow-web/src/components/PublicRoute.jsx
@@ -0,0 +1,30 @@
+import React, {useEffect} from 'react'
+import { Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { getForm } from 'react-formio'
+
+import UserService from '../services/UserService'
+import View from '../components/Form/Item/View'
+import NavBar from "../containers/NavBar";
+
+const PublicRoute =({store})=>{
+
+    useEffect(()=>{
+        UserService.authenticateAnonymousUser(store)
+    },[store])
+    return (
+          <div className="container public-route">
+              <NavBar/>
+              <Route exact path="/public/form/:formId" component={View}/>
+          </div>
+       )
+}
+
+ 
+const mapDispatchToProps = (dispatch,ownProps) => {
+    return {
+        getForm: (id) => dispatch(getForm('form', id))
+    };
+};
+
+export default connect(null, mapDispatchToProps)(PublicRoute);
