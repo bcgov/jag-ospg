270fb3e11f89d29e142b20f2df2fbae9d3caa103
Andre Pestana <andre.pestana@aot-technologies.com>
2022-03-30 16:34:15 -0700

    commented initial intakes and issues

--- api/src/sqlite-example-database/setup.js
+++ api/src/sqlite-example-database/setup.js
@@ -368,174 +368,174 @@ async function reset() {
 			},
 		]);
 
-		await sequelize.models.issue.bulkCreate([
-			{ 
-				issueNumber: "ISSUE-1",
-				applicationId: 3,
-				applicationStatus: "Created",
-				issueName: "test1",
-				issueDescription: "test1",
-				dateOpened: new Date(),
-				issueDetails: null,
-				assignmentId: 1,
-				statusId: 1,
-				closureDate: null,
-				dispositionStatusId: 1,
-				topicId: 1,
-				issueStatusId: 1,
-				initialSourceId: 2,
-				categoryId: 1
-			},
-			{ 
-				issueNumber: "ISSUE-2",
-				applicationId: 5,
-				applicationStatus: "Created",
-				issueName: "test2",
-				issueDescription: "test2",
-				dateOpened: new Date(),
-				issueDetails: null,
-				assignmentId: 1,
-				statusId: 1,
-				closureDate: null,
-				dispositionStatusId: 1,
-				topicId: 1,
-				issueStatusId: 1,
-				initialSourceId: 2,
-				categoryId: 2
-			},
-			{ 
-				issueNumber: "ISSUE-3",
-				applicationId: 7,
-				applicationStatus: "Created",
-				issueName: "test3",
-				issueDescription: "test3",
-				dateOpened: new Date(),
-				issueDetails: null,
-				assignmentId: 1,
-				statusId: 1,
-				closureDate: null,
-				dispositionStatusId: 1,
-				topicId: 1,
-				issueStatusId: 1,
-				initialSourceId: 2,
-				categoryId: 3
-			},
-		]);
+		// await sequelize.models.issue.bulkCreate([
+		// 	{ 
+		// 		issueNumber: "ISSUE-1",
+		// 		applicationId: 3,
+		// 		applicationStatus: "Created",
+		// 		issueName: "test1",
+		// 		issueDescription: "test1",
+		// 		dateOpened: new Date(),
+		// 		issueDetails: null,
+		// 		assignmentId: 1,
+		// 		statusId: 1,
+		// 		closureDate: null,
+		// 		dispositionStatusId: 1,
+		// 		topicId: 1,
+		// 		issueStatusId: 1,
+		// 		initialSourceId: 2,
+		// 		categoryId: 1
+		// 	},
+		// 	{ 
+		// 		issueNumber: "ISSUE-2",
+		// 		applicationId: 5,
+		// 		applicationStatus: "Created",
+		// 		issueName: "test2",
+		// 		issueDescription: "test2",
+		// 		dateOpened: new Date(),
+		// 		issueDetails: null,
+		// 		assignmentId: 1,
+		// 		statusId: 1,
+		// 		closureDate: null,
+		// 		dispositionStatusId: 1,
+		// 		topicId: 1,
+		// 		issueStatusId: 1,
+		// 		initialSourceId: 2,
+		// 		categoryId: 2
+		// 	},
+		// 	{ 
+		// 		issueNumber: "ISSUE-3",
+		// 		applicationId: 7,
+		// 		applicationStatus: "Created",
+		// 		issueName: "test3",
+		// 		issueDescription: "test3",
+		// 		dateOpened: new Date(),
+		// 		issueDetails: null,
+		// 		assignmentId: 1,
+		// 		statusId: 1,
+		// 		closureDate: null,
+		// 		dispositionStatusId: 1,
+		// 		topicId: 1,
+		// 		issueStatusId: 1,
+		// 		initialSourceId: 2,
+		// 		categoryId: 3
+		// 	},
+		// ]);
 
-		await sequelize.models.intake.bulkCreate([
-			{ 
-				issueId: 1,
-				intakeNumber: "INTAKE-1",
-				applicationId: 100,
-				applicationStatus: "Received",
-				intakeTypeId: 1,
-				intakeTitle: "Intake Title",
-				dateReceived: new Date(),
-				contactId: 1,
-				details: 'Test details',
-				responseTypeId: 1,
-				dueDate: 1,
-				resolution: 'Test resolution',
-				intakeStatusId: 1,
-				responseCompleteDate: new Date(),
-				staffAssignedTo: "John Doe",
-				mgmtAssignedTo: "Mary Manager"
-			},
-			{ 
-				issueId: 1,
-				intakeNumber: "INTAKE-2",
-				applicationId: 150,
-				applicationStatus: "Received",
-				intakeTypeId: 1,
-				dateReceived: new Date(),
-				contactId: 1,
-				details: 'Test details',
-				responseTypeId: 1,
-				dueDate: 1,
-				resolution: 'Test resolution',
-				intakeStatusId: 1,
-				responseCompleteDate: new Date(),
-				staffAssignedTo: "John Doe",
-				mgmtAssignedTo: "Mary Manager"
-			},
-			{ 
-				issueId: 2,
-				intakeNumber: "INTAKE-3",
-				applicationId: 200,
-				applicationStatus: "Received",
-				intakeTypeId: 2,
-				dateReceived: new Date(),
-				contactId: 1,
-				details: 'Test details',
-				responseTypeId: 1,
-				dueDate: 1,
-				resolution: 'Test resolution',
-				intakeStatusId: 1,
-				responseCompleteDate: new Date(),
-				staffAssignedTo: "John Doe",
-				mgmtAssignedTo: "Mary Manager"
-			}
-		], {
-			include: [ contact, responseType, intakeStatus, intakeType ]
-		});
+		// await sequelize.models.intake.bulkCreate([
+		// 	{ 
+		// 		issueId: 1,
+		// 		intakeNumber: "INTAKE-1",
+		// 		applicationId: 100,
+		// 		applicationStatus: "Received",
+		// 		intakeTypeId: 1,
+		// 		intakeTitle: "Intake Title",
+		// 		dateReceived: new Date(),
+		// 		contactId: 1,
+		// 		details: 'Test details',
+		// 		responseTypeId: 1,
+		// 		dueDate: 1,
+		// 		resolution: 'Test resolution',
+		// 		intakeStatusId: 1,
+		// 		responseCompleteDate: new Date(),
+		// 		staffAssignedTo: "John Doe",
+		// 		mgmtAssignedTo: "Mary Manager"
+		// 	},
+		// 	{ 
+		// 		issueId: 1,
+		// 		intakeNumber: "INTAKE-2",
+		// 		applicationId: 150,
+		// 		applicationStatus: "Received",
+		// 		intakeTypeId: 1,
+		// 		dateReceived: new Date(),
+		// 		contactId: 1,
+		// 		details: 'Test details',
+		// 		responseTypeId: 1,
+		// 		dueDate: 1,
+		// 		resolution: 'Test resolution',
+		// 		intakeStatusId: 1,
+		// 		responseCompleteDate: new Date(),
+		// 		staffAssignedTo: "John Doe",
+		// 		mgmtAssignedTo: "Mary Manager"
+		// 	},
+		// 	{ 
+		// 		issueId: 2,
+		// 		intakeNumber: "INTAKE-3",
+		// 		applicationId: 200,
+		// 		applicationStatus: "Received",
+		// 		intakeTypeId: 2,
+		// 		dateReceived: new Date(),
+		// 		contactId: 1,
+		// 		details: 'Test details',
+		// 		responseTypeId: 1,
+		// 		dueDate: 1,
+		// 		resolution: 'Test resolution',
+		// 		intakeStatusId: 1,
+		// 		responseCompleteDate: new Date(),
+		// 		staffAssignedTo: "John Doe",
+		// 		mgmtAssignedTo: "Mary Manager"
+		// 	}
+		// ], {
+		// 	include: [ contact, responseType, intakeStatus, intakeType ]
+		// });
 
-		await sequelize.models.attachment.bulkCreate([
-			{ 
-				intakeId: 1,
-				url: "https://bc.gov.ca/api/v1/files/test-567888-9876987-987688-876876.pdf?originalName=test.pdf",
-				name: "test-567888-9876987-987688-876876.pdf",
-				originalName: "test.pdf",
-				size: 1024
-			}
-		]);
+		// await sequelize.models.attachment.bulkCreate([
+		// 	{ 
+		// 		intakeId: 1,
+		// 		url: "https://bc.gov.ca/api/v1/files/test-567888-9876987-987688-876876.pdf?originalName=test.pdf",
+		// 		name: "test-567888-9876987-987688-876876.pdf",
+		// 		originalName: "test.pdf",
+		// 		size: 1024
+		// 	}
+		// ]);
 
-		await sequelize.models.attachment.bulkCreate([
-			{ 
-				intakeId: 1,
-				dateAdded: new Date(),
-				fileName: 'document2.doc',
-				fileType: 'Contract'
-			}
-		]);
+		// await sequelize.models.attachment.bulkCreate([
+		// 	{ 
+		// 		intakeId: 1,
+		// 		dateAdded: new Date(),
+		// 		fileName: 'document2.doc',
+		// 		fileType: 'Contract'
+		// 	}
+		// ]);
 
 
-		await sequelize.models.issueRegulatoryBody.bulkCreate([
-			{ 
-				issueId: 1,
-				regulatoryBodyId: 1
-			}
-		]);
-		await sequelize.models.note.bulkCreate([
-			{ 
-				intakeId: 1,
-				body: 'Body... ',
-				username: 'user1',
-				id: "567888-9876987-987688-876876"
-			},
-			{ 
-				intakeId: 2,
-				body: 'Body2... ',
-				username: 'user2',
-				id: "567888-9876987-987688-87333"
-			}
-		]);
+		// await sequelize.models.issueRegulatoryBody.bulkCreate([
+		// 	{ 
+		// 		issueId: 1,
+		// 		regulatoryBodyId: 1
+		// 	}
+		// ]);
+		// await sequelize.models.note.bulkCreate([
+		// 	{ 
+		// 		intakeId: 1,
+		// 		body: 'Body... ',
+		// 		username: 'user1',
+		// 		id: "567888-9876987-987688-876876"
+		// 	},
+		// 	{ 
+		// 		intakeId: 2,
+		// 		body: 'Body2... ',
+		// 		username: 'user2',
+		// 		id: "567888-9876987-987688-87333"
+		// 	}
+		// ]);
 
-		await sequelize.models.communicationLog.bulkCreate([
-			{ 
-				intakeId: 1,
-				subject: 'Subject',
-				body: 'Body... ',
-				from: 'User Test',
-				id: "567888-9876987-987688-99999"
-			},
-			{ 
-				intakeId: 2,
-				subject: 'Subject2',
-				body: 'Body2... ',
-				from: 'User Test',
-				id: "567888-9876987-987688-93339"
-			}
-		]);
+		// await sequelize.models.communicationLog.bulkCreate([
+		// 	{ 
+		// 		intakeId: 1,
+		// 		subject: 'Subject',
+		// 		body: 'Body... ',
+		// 		from: 'User Test',
+		// 		id: "567888-9876987-987688-99999"
+		// 	},
+		// 	{ 
+		// 		intakeId: 2,
+		// 		subject: 'Subject2',
+		// 		body: 'Body2... ',
+		// 		from: 'User Test',
+		// 		id: "567888-9876987-987688-93339"
+		// 	}
+		// ]);
 	} catch (e) {
 		if (e instanceof Sequelize.ValidationError) {
 			console.log(e.errors);
