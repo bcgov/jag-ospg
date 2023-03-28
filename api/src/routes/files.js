b982e2f8ba9c3584c47ce39cb4acd8f84531608b
Vinod Uppinipurath <vinod@aot-technologies.com>
2023-03-27 18:14:45 -0700

    Update files.js
    debug

--- api/src/routes/files.js
+++ api/src/routes/files.js
@@ -21,15 +21,19 @@ uploadFile = async (req, res) => {
     const fileSize = req.files.file.size;
     const fileMimetype = req.files.file.mimetype;
     const fileS3Name = req.body.name; // to be sent to s3
+    
+    console.log('file.js Received Request to upload a file' - fileS3Name);
 
     try {
         const s3Resp = await s3UploadFile(fileS3Name, 
                                     fileData, 
                                     fileSize, 
                                     fileMimetype);
+    console.log('file.js upload file before completion');
     } catch (error) {
         res.status(500).send("Error while uploading file.");
     }
+    console.log('file.js upload file completed');
     res.status(201).json({
         "status": "Ok",
         "url": `https://${req.headers.host}/api/v1/files/${fileS3Name}?originalName=${fileOriginalName}`
