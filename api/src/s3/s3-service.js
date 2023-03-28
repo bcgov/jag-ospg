1f885ddb99e49baf13487be44a80ad342f1bd71c
Vinod Uppinipurath <vinod@aot-technologies.com>
2023-03-27 19:37:19 -0700

    Update s3-service.js
    more debug

--- api/src/s3/s3-service.js
+++ api/src/s3/s3-service.js
@@ -42,11 +42,23 @@ const s3UploadFile = async (fileS3Name,
      console.log('File uploaded  and response is - ',resp);
         return resp;
     } catch (error) {
-             console.log('s3-service.js axios call failed',error.toJSON());
-        console.log('error.response.data',error.response.data);
-        console.log('error.response',error.response);
-        console.log('error',error);
-        throw error
+        console.log('s3-service.js axios call failed',error.toJSON());
+        if (error.response) {
+              // The request was made and the server responded with a status code
+              // that falls out of the range of 2xx
+              console.log(error.response.data);
+              console.log(error.response.status);
+              console.log(error.response.headers);
+            } else if (error.request) {
+              // The request was made but no response was received
+              // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+              // http.ClientRequest in node.js
+              console.log(error.request);
+            } else {
+              // Something happened in setting up the request that triggered an Error
+              console.log('Error', error.message);
+            }
+        throw error;
     }
 }
 
